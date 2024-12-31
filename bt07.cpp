78ce99ae10bbc79906efaaf394e6e1d8c7590e4d
MinhKhoi1706 <nguyenminhkhoi17062006@gmail.com>
2024-12-31 15:07:07 +0700

    Add files via upload

--- bt07.cpp
+++ bt07.cpp
@@ -1,54 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define max 100
 
-int** createMatrix(int rows, int cols) {
-    int** matrix = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int*)malloc(cols * sizeof(int));
-    }
+void printArray(int col, int row);
+void initArray(int col, int row);
 
-    printf("nhap cac phan tu cua ma tran:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("nhap phan tu a[%d][%d]: ", i + 1, j + 1);
+int matrix[max][max];
+
+int main(){
+    int col = 3, row = 4;
+
+    initArray(col, row);
+    printArray(col, row);
+
+    return 0;
+}
+
+void initArray(int col, int row){
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            printf("matrix[%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
-
-    return matrix;
 }
 
-void printMatrix(int** matrix, int rows, int cols) {
-    printf("ma tran da nhap:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+void printArray(int col, int row){
+    printf("\n");
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
-}
-
-void freeMatrix(int** matrix, int rows) {
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-}
-
-int main() {
-    int rows, cols;
-
-    printf("nhap so hang: ");
-    scanf("%d", &rows);
-    printf("nhap so cot: ");
-    scanf("%d", &cols);
-
-    int** matrix = createMatrix(rows, cols);
-
-    printMatrix(matrix, rows, cols);
-
-    freeMatrix(matrix, rows);
-
-    return 0;
 }
 
